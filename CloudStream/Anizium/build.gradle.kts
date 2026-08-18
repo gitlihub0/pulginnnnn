df21091aee4feae40571cf6c290ef3b63a154dbb
gitlihub0 <git0hub00@proton.me>
2026-08-18 12:13:59 +0300

    Add files via upload

---
+++ CloudStream/Anizium/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("qwert")
+    language    = "tr"
+    description = "4K Anime izleme platformu Anizium, binlerce anime içeriğini Türkçe altyazı/dublaj ve çoklu dil seçenekleriyle sunar."
+    status      = 1
+    tvTypes     = listOf("Anime", "AnimeMovie", "OVA")
+    iconUrl     = "https://anizium.com/assets/images/favicon.png"
+}
